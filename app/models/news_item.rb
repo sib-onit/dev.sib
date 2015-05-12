13a8fb477245e682ccb493fbd2f0b021b3139ee7
root <root@hagen.life.illinois.edu>
2015-05-12 08:39:49 -0500

    Before changing the supermenu content

--- app/models/news_item.rb
+++ app/models/news_item.rb
@@ -1,8 +1,8 @@
 class NewsItem < ActiveRecord::Base
 
 	has_many :comments, dependent: :destroy
-	validates :title, presence: true,
-			  length: {minimum: 5}
+#	validates :title, presence: true,
+#			  length: {minimum: 5}
 
 
 end
